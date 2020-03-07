ded3d750cd229b4860ba7bc9edf02759e47644d2
sidorkinandrew <47028236+sidorkinandrew@users.noreply.github.com>
2020-03-07 21:54:33 +0000

    Update 05-re.py

--- diveintopython3/05-re.py
+++ diveintopython3/05-re.py
@@ -1,5 +1,6 @@
+=========================================================
 >>> pattern = '''
-    ^                   # beginning of string
+    ^                   # ROMAN NUMBERS: beginning of string
     M{0,3}              # thousands - 0 to 3 Ms
     (CM|CD|D?C{0,3})    # hundreds - 900 (CM), 400 (CD), 0-300 (0 to 3 Cs),
                         #            or 500-800 (D, followed by 0 to 3 Cs)
@@ -10,3 +11,17 @@
     $                   # end of string
     '''
 >>> re.search(pattern, 'M', re.VERBOSE)
+=========================================================
+>>> phonePattern = re.compile(r'''
+                # USA TELEPHONE NUMBERS: don't match beginning of string, number can start anywhere
+    (\d{3})     # area code is 3 digits (e.g. '800')
+    \D*         # optional separator is any number of non-digits
+    (\d{3})     # trunk is 3 digits (e.g. '555')
+    \D*         # optional separator
+    (\d{4})     # rest of number is 4 digits (e.g. '1212')
+    \D*         # optional separator
+    (\d*)       # extension is optional and can be any number of digits
+    $           # end of string
+    ''', re.VERBOSE)
+>>> phonePattern.search('work 1-(800) 555.1212 #1234').groups() ①
+('800', '555', '1212', '1234')
