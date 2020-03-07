81d949191eb754bd62054105e187cb00d4455945
sidorkinandrew <47028236+sidorkinandrew@users.noreply.github.com>
2020-03-07 21:14:24 +0000

    Update and rename strings.py to 04-strings.py

---
+++ diveintopython3/04-strings.py
@@ -0,0 +1,12 @@
+=======================================================================
+>>> import humansize
+>>> import sys
+>>> '1MB = 1000{0.modules[humansize].SUFFIXES[1000][0]}'.format(sys)
+'1MB = 1000KB'
+=======================================================================
+>> a_list_of_lists
+[['user', 'pilgrim'], ['database', 'master'], ['password', 'PapayaWhip']]
+>>> a_dict = dict(a_list_of_lists)
+>>> a_dict
+{'password': 'PapayaWhip', 'user': 'pilgrim', 'database': 'master'}
+=======================================================================
