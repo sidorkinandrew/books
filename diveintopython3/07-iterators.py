684b6f5e208090857731866332281230f52533e0
sidorkinandrew <47028236+sidorkinandrew@users.noreply.github.com>
2020-03-09 21:43:07 +0000

    Update 07-iterators.py

--- diveintopython3/07-iterators.py
+++ diveintopython3/07-iterators.py
@@ -4,7 +4,7 @@
 # acts like one (it’s the first piece of code executed in a newly created instance of the class), and even sounds like one.
 # Incorrect, because the object has already been constructed by the time the __init__() method is called,
 # and you already have a valid reference to the new instance of the class.
-
+# ====================================================================================
 class Fib:
     '''iterator that yields numbers in the Fibonacci sequence'''
 
@@ -22,3 +22,40 @@ class Fib:
             raise StopIteration
         self.a, self.b = self.b, self.a + self.b
         return fib
+
+# ====================================================================================
+class LazyRules:
+    '''Iterator for pluralization rules for English words 
+    
+    '''
+    
+    rules_filename = 'plural6-rules.txt'
+
+    def __init__(self):
+        self.pattern_file = open(self.rules_filename, encoding='utf-8')
+        self.cache = []
+
+    def __iter__(self):
+        self.cache_index = 0
+        return self
+
+    def __next__(self):
+        self.cache_index += 1
+        if len(self.cache) >= self.cache_index:
+            return self.cache[self.cache_index - 1]
+
+        if self.pattern_file.closed:
+            raise StopIteration
+
+        line = self.pattern_file.readline()
+        if not line:
+            self.pattern_file.close()
+            raise StopIteration
+
+        pattern, search, replace = line.split(None, 3)
+        funcs = build_match_and_apply_functions(
+            pattern, search, replace)
+        self.cache.append(funcs)
+        return funcs
+
+rules = LazyRules()
