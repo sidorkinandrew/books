d55df5caea39bc30830b45ea77a647bd77fdbf84
sidorkinandrew <47028236+sidorkinandrew@users.noreply.github.com>
2020-03-09 22:33:14 +0000

    Create 09-unit-testing.py

---
+++ diveintopython3/09-unit-testing.py
@@ -0,0 +1,5 @@
+#A test case answers a single question about the code it is testing. A test case should be able to...
+#...run completely by itself, without any human input. Unit testing is about automation.
+#...determine by itself whether the function it is testing has passed or failed, without a human interpreting the results.
+#...run in isolation, separate from any other test cases (even if they test the same functions). Each test case is an island.
+
