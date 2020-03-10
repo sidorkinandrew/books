50c426257e37265c3dd0e5d9d9cc967e7828812a
sidorkinandrew <47028236+sidorkinandrew@users.noreply.github.com>
2020-03-10 23:00:12 +0000

    Update 09-unit-testing.py

--- diveintopython3/09-unit-testing.py
+++ diveintopython3/09-unit-testing.py
@@ -3,3 +3,4 @@
 #...determine by itself whether the function it is testing has passed or failed, without a human interpreting the results.
 #...run in isolation, separate from any other test cases (even if they test the same functions). Each test case is an island.
 
+# Every individual test is its own method. A test method takes no parameters, returns no value, and must have a name beginning with the four letters test. If a test method exits normally without raising an exception, the test is considered passed; if the method raises an exception, the test is considered failed.
