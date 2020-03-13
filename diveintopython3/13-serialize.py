580bcae84726e211f95f91791d4d97b4e68ce569
sidorkinandrew <47028236+sidorkinandrew@users.noreply.github.com>
2020-03-13 23:43:53 +0000

    Update 13-serialize.py

--- diveintopython3/13-serialize.py
+++ diveintopython3/13-serialize.py
@@ -11,3 +11,35 @@ real number	float
 *	null	None
 
 * All json values are case-sensitive.
+
+
+
+import time
+
+def to_json(python_object):
+    if isinstance(python_object, time.struct_time):          
+        return {'__class__': 'time.asctime',
+                '__value__': time.asctime(python_object)}    
+    if isinstance(python_object, bytes):
+        return {'__class__': 'bytes',
+                '__value__': list(python_object)}
+    raise TypeError(repr(python_object) + ' is not JSON serializable')
+    
+>>> with open('entry.json', 'w', encoding='utf-8') as f:
+...     json.dump(entry, f, default=customserializer.to_json)
+
+# add this to customserializer.py
+def from_json(json_object):                                   
+    if '__class__' in json_object:                            
+        if json_object['__class__'] == 'time.asctime':
+            return time.strptime(json_object['__value__'])    
+        if json_object['__class__'] == 'bytes':
+            return bytes(json_object['__value__'])            
+    return json_object
+
+>>> import customserializer
+>>> with open('entry.json', 'r', encoding='utf-8') as f:
+...  entry = json.load(f, object_hook=customserializer.from_json)
+
+
+#  the value of the 'tags' key is a list of three strings. json doesn’t distinguish between tuples and lists; it only has a single list-like datatype, the array, and the json module silently converts both tuples and lists into json arrays during serialization. For most uses, you can ignore the difference between tuples and lists, but it’s something to keep in mind as you work with the json module.
