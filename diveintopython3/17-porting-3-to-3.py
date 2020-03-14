e01a62f8d93479ce245d2ab297712e57bc94a86d
sidorkinandrew <47028236+sidorkinandrew@users.noreply.github.com>
2020-03-14 10:00:50 +0000

    Create 17-porting-3-to-3.py

---
+++ diveintopython3/17-porting-3-to-3.py
@@ -0,0 +1,53 @@
+ Porting Code to Python 3 with 2to3
+Diving In
+print statement
+Unicode string literals
+unicode() global function
+long data type
+<> comparison
+has_key() dictionary method
+Dictionary methods that return lists
+Modules that have been renamed or reorganized
+http
+urllib
+dbm
+xmlrpc
+Other modules
+Relative imports within a package
+next() iterator method
+filter() global function
+map() global function
+reduce() global function
+apply() global function
+intern() global function
+exec statement
+execfile statement
+repr literals (backticks)
+try...except statement
+raise statement
+throw method on generators
+xrange() global function
+raw_input() and input() global functions
+func_* function attributes
+xreadlines() I/O method
+lambda functions that take a tuple instead of multiple parameters
+Special method attributes
+__nonzero__ special method
+Octal literals
+sys.maxint
+callable() global function
+zip() global function
+StandardError exception
+types module constants
+isinstance() global function
+basestring datatype
+itertools module
+sys.exc_type, sys.exc_value, sys.exc_traceback
+List comprehensions over tuples
+os.getcwdu() function
+Metaclasses
+Matters of style
+set() literals (explicit)
+buffer() global function (explicit)
+Whitespace around commas (explicit)
+Common idioms (explicit)
