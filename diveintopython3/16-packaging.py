3639b00f8cf2be7e04f2ecd4514ef0adf6203231
sidorkinandrew <47028236+sidorkinandrew@users.noreply.github.com>
2020-03-14 09:58:22 +0000

    Create 16-packaging.py

---
+++ diveintopython3/16-packaging.py
@@ -0,0 +1,14 @@
+Packaging Python Libraries
+Diving In
+Things Distutils Can’t Do For You
+Directory Structure
+Writing Your Setup Script
+Classifying Your Package
+Examples of Good Package Classifiers
+Specifying Additional Files With A Manifest
+Checking Your Setup Script for Errors
+Creating a Source Distribution
+Creating a Graphical Installer
+Building Installable Packages for Other Operating Systems
+Adding Your Software to The Python Package Index
+The Many Possible Futures of Python Packaging
