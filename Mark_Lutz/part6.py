2c0ad82426a14faadb6c189f84e64bf0b6cc5b39
sidorkinandrew <47028236+sidorkinandrew@users.noreply.github.com>
2020-03-15 12:28:14 +0000

    Update part6.py

--- Mark_Lutz/part6.py
+++ Mark_Lutz/part6.py
@@ -2,4 +2,5 @@
 >>> x. __ diet __ [ ’ age 1 ] # Индексирование словаря не производит поиск в иерархии наследования
 >>> х. __ class __ # Связь экземпляра с классом
 >>> гес. __ bases __ # Связь с суперклассами
-
+# Fetching all methds/properties - use __class__ and then ___dict__, then __dict__ against all __bases__ 
+# OR use dir(obj)
