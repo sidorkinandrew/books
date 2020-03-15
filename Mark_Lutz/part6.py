3592a5cb7b530a64d561bfac09f2c12ca0172d54
sidorkinandrew <47028236+sidorkinandrew@users.noreply.github.com>
2020-03-15 12:28:42 +0000

    Update part6.py

--- Mark_Lutz/part6.py
+++ Mark_Lutz/part6.py
@@ -4,3 +4,7 @@
 >>> гес. __ bases __ # Связь с суперклассами
 # Fetching all methds/properties - use __class__ and then ___dict__, then __dict__ against all __bases__ 
 # OR use dir(obj)
+>>> len(dir(bob) )
+32
+>>> list (name for name in dir (bob) if not name, start s with (' __ '))
+['gatherAttrs', 'giveRaise', 'job', 'lastName', 'name', 'pay']
