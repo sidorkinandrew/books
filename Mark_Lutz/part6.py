a85618abbf453a19f2132da729a0edce73a40f19
sidorkinandrew <47028236+sidorkinandrew@users.noreply.github.com>
2020-03-15 12:35:18 +0000

    Update part6.py

--- Mark_Lutz/part6.py
+++ Mark_Lutz/part6.py
@@ -9,3 +9,12 @@
 >>> list (name for name in dir (bob) if not name, start s with (' __ '))
 ['gatherAttrs', 'giveRaise', 'job', 'lastName', 'name', 'pay']
 # __some_method - pseudo-closed attributes of a class
+
+def gatherAttrs(self):
+    attrs = []
+    for key in sorted (self.__dict__):
+        attrs.append ('{}={}'.format(key, getattr(self,key))
+    return ', '.join(attrs)
+
+def __repr__ (self) :
+    return '[%{}: %{}]'.format(self.__class__.__name__, self.gatherAttrs())
