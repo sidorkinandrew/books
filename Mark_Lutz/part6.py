eee604c2d1df88e68c86d9607842c5de0c33abbc
sidorkinandrew <47028236+sidorkinandrew@users.noreply.github.com>
2020-03-16 09:42:35 +0000

    Update part6.py

--- Mark_Lutz/part6.py
+++ Mark_Lutz/part6.py
@@ -103,3 +103,25 @@ class SkipObject:
 # __bool__ >= __len__
 
 # def/lambda | __call__ | bound_function | classmethod | staticmethod
+
+
+# p. 190 - factories
+def factory(aClass, *pargs, **kargs):
+    return aClass(*pargs, **kargs)
+
+class Spam:
+    def doit(self, message):
+        print(message)
+
+class Person:
+    def __init__(self, name, job=None):
+        self.name = name
+        self.job = job
+
+objectl = factory(Spam)
+object2 = factory(Person, "Arthur", "King")
+object3 = factory(Person, name='Brian')
+objectl.doit(99)
+print(object2.name, object2.job)
+print(object3.name, object3.job)
+
