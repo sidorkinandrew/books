c7d2f4ca86e9773b4c1d8dbdf4b99768e8d28301
git_admin <sidorkina.oksana.19@gmail.com>
2020-03-20 23:06:18 +0000

    Update part6-tasks.py

--- Mark_Lutz/part6-tasks.py
+++ Mark_Lutz/part6-tasks.py
@@ -48,3 +48,90 @@ class MyListSub(MyList):
         return __superstr + f' "Add" calls done: {self.count_calls}'
 
 
+# task #7 - FastFood Restaurant
+class Lunch:
+    def __init__(self):
+        """
+        instantiates Customer, Employee
+        """
+        self.customer = Customer()
+        self.employee = Employee()
+    def orderQueue(self, *args):
+        """
+        places orders for a customer
+        :param args:
+        :return:
+        """
+        for foodItem in args:
+            self.order(foodItem)
+    def order(self, foodName):
+        """
+        Customer buys foodName
+        :param foodName:
+        :return:
+        """
+        self.customer.placeOrder(foodName, self.employee)
+    def result(self):
+        """
+        Customer asks for instance of food
+        :return:
+        """
+        return self.customer.printFood()
+
+
+class Customer:
+    meal = None
+    def __init__(self):
+        """
+        initializes meal
+        """
+        self.meal = []
+    def placeOrder(self, foodName, employee):
+        """
+        places an order with employee instance
+        :param foodName:
+        :param employee:
+        :return:
+        """
+        self.meal.append(employee.takeOrder(foodName))
+    def printFood(self):
+        """
+        prints foodName
+        :return:
+        """
+        return [item.foodName for item in self.meal]
+    def __repr__(self):
+        """
+        debugging str
+        :return:
+        """
+        return str([item.foodName for item in self.meal])
+
+class Employee:
+    processingQueue = None
+    def __init__(self):
+        """
+        Queue for an employee
+        """
+        self.processingQueue = []
+    def takeOrder(self, foodName):
+        """
+        returns Food instance with the given foodName
+        :param foodName:
+        :return:
+        """
+        self.processingQueue.append(Food(foodName))
+        return self.processingQueue[-1]
+
+
+class Food:
+    foodName = None
+    def __init__(self, foodName):
+        """
+        saves the foodName
+        """
+        self.foodName = foodName
+
+
+# a = Lunch(); a.order("burito")
+# a.orderQueue('nachos','cola','potatoes','sause','shawerma')
