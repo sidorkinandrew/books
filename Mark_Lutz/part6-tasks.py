0fe1c56a6867d885db3aef0161f46157b46594b3
git_admin <sidorkina.oksana.19@gmail.com>
2020-03-20 20:08:38 +0000

    Update part6-tasks.py

--- Mark_Lutz/part6-tasks.py
+++ Mark_Lutz/part6-tasks.py
@@ -1,3 +1,4 @@
+# task #2
 class MyList(list):
     data = None
 
@@ -31,3 +32,17 @@ class MyList(list):
 
     def add(self, alist: list):
         self.data = self.data + alist
+
+# task #3
+class MyListSub(MyList):
+    count_calls = 0
+    def __init__(self, list_object):
+        self.count_calls = 0
+        MyList.__init__(self,list_object)
+    def __add__(self, other):
+        self.count_calls += 1
+        print(f'Calls done: {self.count_calls}')
+        MyList.add(self, other)
+    def __repr__(self):
+        _super_str = MyList.__repr__(self)
+        return _super_str + f' "Add" calls done: {self.count_calls}'
