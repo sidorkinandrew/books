784cd53e898ff52ad47ea42863a841c7cae06224
git_admin <sidorkina.oksana.19@gmail.com>
2020-03-23 22:45:14 +0000

    Update part7.py

--- Mark_Lutz/part7.py
+++ Mark_Lutz/part7.py
@@ -4,3 +4,21 @@
 # raise
 # assert
 # with/as
+# except Exception
+# try/except/else/finally
+
+class MyError(Exception): pass
+
+def stuff(file):
+    raise MyError()
+
+file = open ('data' , 'w')  # may fail here
+
+try:
+    stuff(file)  # Exception MyError is generated
+finally:
+    file.close ()  # cleanup phase
+
+print('not reached')  # MyError will be propagated
+
+# try must have either except OR finally
