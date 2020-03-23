c21c18dff344bc00886436d2e928e3869dfe78b9
git_admin <sidorkina.oksana.19@gmail.com>
2020-03-23 23:49:16 +0000

    Update part7.py

--- Mark_Lutz/part7.py
+++ Mark_Lutz/part7.py
@@ -22,3 +22,56 @@ finally:
 print('not reached')  # MyError will be propagated
 
 # try must have either except OR finally
+
+# raise new-exception from other Exception
+
+try:
+   try:
+      raise IndexError ()
+   except Exception as E :
+      raise TypeError () from E
+except Exception as E :
+   raise SyntaxError () from E
+
+# raise from None
+
+assert test, data
+# "equals" to
+if __debug__:
+    if not test:
+        raise AssertionError (data)
+
+# python -O main.py => Optimized run with __debug__ = False
+def f (х) :
+    assert х < 0, 'х must be negative'
+    return х ** 2
+
+import asserter
+asserter.f(1)
+
+with decimal.localcontext() as ctx:  # import decimal
+    ctx.prec = 2
+    x = decimal.Decimal('1.00')/decimal.Decimal('3.00')
+
+class TraceBlock:
+    def message(self , arg) :
+        print (' running ' + arg)
+    def __ enter __ (self) :
+        print (' starting with block')
+        return self
+    def __ exit __ (self, exc_type, exc_value, exc_tb) :
+        if exc_type is None:
+            print (' exited normally! ') # normal exit
+        else :
+            print ('raise an exception! ' + str (exc_type) ) # generated Exception
+        return False # propagated
+if __ name __ == ' __ main __ ' :
+    with TraceBlock () as action:
+        action.message (' test 1')
+        print (' reached' ) 
+    with TraceBlock () as action:
+        action.message (' test 2')
+        raise TypeError
+        print('not reached') 
+
+
