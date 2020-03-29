178ef3305e140a97ebef934076ece4a4db2d5816
git_admin <sidorkina.oksana.19@gmail.com>
2020-03-29 00:33:01 +0000

    Update part7.py

--- Mark_Lutz/part7.py
+++ Mark_Lutz/part7.py
@@ -192,3 +192,18 @@ class D:
 # __delete__ (__delattr__) <> __del__  !!!
 
 
+class Name:
+    "name descriptor docs"
+    def __get__(self, instance, owner) :
+        print (' fetch ...')
+        return instance._name
+    def __set__(self, instance, value) :
+        print ( ’ change . . . ')
+        instance._name = value
+    def __delete__(self, instance) :
+        print (' remove ...')
+        del instance._name
+class Person:
+    def __init__ (self, name) :
+        self._name = name
+    name = Name ()
