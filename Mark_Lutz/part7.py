aa357b382da8fd8cf66d0fe88032ee2b49ff7a79
git_admin <sidorkina.oksana.19@gmail.com>
2020-03-29 21:33:55 +0000

    Update part7.py

--- Mark_Lutz/part7.py
+++ Mark_Lutz/part7.py
@@ -207,3 +207,22 @@ class Person:
     def __init__ (self, name) :
         self._name = name
     name = Name ()
+
+
+# nested descriptor
+class Person:
+    def __init__(self, name):
+        self._name = name
+
+    class Name:                                 # Using a nested class
+        "name descriptor docs"
+        def __get__(self, instance, owner):
+            print('fetch...')
+            return instance._name
+        def __set__(self, instance, value):
+            print('change...')
+            instance._name = value
+        def __delete__(self, instance):
+            print('remove...')
+            del instance._name
+    name = Name()
